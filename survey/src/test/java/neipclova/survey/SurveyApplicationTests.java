6b2588da809f2bb920b3f0065f009f37ec4addb8
yeonsoo <ala8343@gmail.com>
2022-03-09 20:18:05 +0900

    feat: initialize spring boot

---
+++ survey/src/test/java/neipclova/survey/SurveyApplicationTests.java
@@ -0,0 +1,13 @@
+package neipclova.survey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SurveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

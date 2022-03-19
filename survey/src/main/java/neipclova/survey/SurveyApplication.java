c6d0fe2f9b7387486d50c761906cd0648c7c5196
2hwayoung <hwa3270@gmail.com>
2022-03-20 05:56:30 +0900

    feat: create member example

--- survey/src/main/java/neipclova/survey/SurveyApplication.java
+++ survey/src/main/java/neipclova/survey/SurveyApplication.java
@@ -2,10 +2,11 @@ package neipclova.survey;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing //시간에 대해서 자동으로 값을 넣어주는 기능
 @SpringBootApplication
 public class SurveyApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(SurveyApplication.class, args);
 	}

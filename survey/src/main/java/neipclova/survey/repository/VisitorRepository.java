928b7804c68a06bc124971c66d42741af5fc95cc
yonjaeee <yonjae@yonsei.ac.kr>
2022-06-20 22:36:05 +0900

    feat: added api except for result logic

--- survey/src/main/java/neipclova/survey/repository/VisitorRepository.java
+++ survey/src/main/java/neipclova/survey/repository/VisitorRepository.java
@@ -2,6 +2,10 @@ package neipclova.survey.repository;
 
 import neipclova.survey.domain.Visitor;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface VisitorRepository extends JpaRepository<Visitor, Long> {
+    //@Query("select count(distinct v ) from Visitor v")
+    //Integer countDistinct();
 }

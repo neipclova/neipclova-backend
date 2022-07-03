4c30ccd5c815678bec3ad6fde4a0d26a02b1c232
USER <user@AL01549694.local>
2022-07-04 00:03:53 +0900

    [working] temporary works

--- survey/src/main/java/neipclova/survey/controller/ResultController.java
+++ survey/src/main/java/neipclova/survey/controller/ResultController.java
@@ -1,26 +1,37 @@
 package neipclova.survey.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import neipclova.survey.domain.Visitor;
+import neipclova.survey.domain.VisitorSurveyAnswer;
+import neipclova.survey.domain.VisitorSurveyResult;
+import neipclova.survey.repository.VisitorSurveyAnswerRepository;
+import neipclova.survey.service.AnswerService;
+import neipclova.survey.service.ResultService;
 import neipclova.survey.service.StartService;
 
 public class ResultController {
-    private StartService startService;
 
-    @PostMapping ("/result/{survey_type}")
-    public ResponseEntity<Map<String, Object>> addVisitor(@RequestBody Visitor visitor) {
-        startService.saveVisitor(visitor);
-        Long visitor_id = visitor.getId();
-        Map<String, Object> result = new HashMap<>();
-        result.put("visitor_id", visitor_id);
-        return ResponseEntity.ok().body(result);
+    private ResultService resultService;
+    private AnswerService answerService;
+    private VisitorSurveyAnswerRepository visitorSurveyAnswerRepository;
+    @PostMapping("/result/{survey_type}")
+    public void SurveyResult(@RequestBody VisitorSurveyResult vsr) {
+
+        int[] mbti = {0, 0, 0, 0};
+        List<VisitorSurveyAnswer> visitorAnswers = visitorSurveyAnswerRepository.findByByVisitorId(vsr.getVisitor_id());
+
+
+
+        resultService.addResult();
     }
 
 }

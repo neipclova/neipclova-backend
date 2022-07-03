9364d41e97e815e4186b5bdc716e375d14b0c4b4
USER <user@AL01549694.local>
2022-07-04 00:03:53 +0900

    [working] temporary works

--- survey/src/main/java/neipclova/survey/repository/VisitorSurveyAnswerRepository.java
+++ survey/src/main/java/neipclova/survey/repository/VisitorSurveyAnswerRepository.java
@@ -1,8 +1,37 @@
 package neipclova.survey.repository;
 
-import neipclova.survey.domain.VisitorSurveyAnswer;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
 import org.springframework.stereotype.Repository;
 
-public interface VisitorSurveyAnswerRepository extends JpaRepository<VisitorSurveyAnswer, Long> {
+import lombok.RequiredArgsConstructor;
+import neipclova.survey.domain.Answer;
+import neipclova.survey.domain.Member;
+import neipclova.survey.domain.VisitorSurveyAnswer;
+import neipclova.survey.domain.VisitorSurveyResult;
+
+@Repository
+@RequiredArgsConstructor
+public class VisitorSurveyAnswerRepository {
+
+    private final EntityManager em;
+
+    public List<VisitorSurveyAnswer> findByByVisitorId(Long visitor_id) {
+        return em.createQuery("select question, answer from VisitorSurveyAnswer where visitor=:visitor_id",VisitorSurveyAnswer.class)
+                .setParameter("visitor_id",visitor_id)
+                .getResultList();
+    }
+
+    public List<Answer> scoreByVisitorId(Long visitor_id) {
+        return em.createQuery("select score from Answer as a left join VisitorSurveyAnswer as vsa on a.answer_order = vsa.answer and a.question = vsa.question where vsa.visitor=:visitor_id",
+                              Answer.class)
+                 .setParameter("visitor_id",visitor_id)
+                 .getResultList();
+    }
+
+    public void save(VisitorSurveyAnswer visitorSurveyAnswer) {
+        em.persist(visitorSurveyAnswer);
+    }
 }

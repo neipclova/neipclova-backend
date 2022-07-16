85dd8a6283b71e3d03010f4cf32557e9c3df8c82
USER <user@AL01549694.local>
2022-07-17 00:41:13 +0900

    [fix] fix missing bean contorll on ResultService

--- survey/src/main/java/neipclova/survey/service/ResultService.java
+++ survey/src/main/java/neipclova/survey/service/ResultService.java
@@ -1,14 +1,17 @@
 package neipclova.survey.service;
 
+import lombok.RequiredArgsConstructor;
 import neipclova.survey.domain.VisitorSurveyAnswer;
 import neipclova.survey.domain.VisitorSurveyResult;
 import neipclova.survey.repository.VisitorSurveyAnswerRepository;
 import neipclova.survey.repository.VisitorSurveyResultRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+@Service
+@RequiredArgsConstructor
 public class ResultService {
-    @Autowired
     private VisitorSurveyResultRepository visitorSurveyResultRepository;
 
     @Transactional
